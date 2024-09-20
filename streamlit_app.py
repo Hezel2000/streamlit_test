e826795dd6e626089c12f4b1a4f428b8d074f5ac
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2024-09-20 12:55:31 +0000

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 st.title('Hello World')
 
@@ -10,9 +10,19 @@ df = pd.read_csv('data/Bastar Craton.csv')
 
 cat_names = df.columns.tolist()[27:]
 
-el1 = st.selectbox('select element', cat_names = df.columns.tolist()[27:]
-)
-st.write(el1)
+col1, col2 = st.columns(2)
+
+st.multiselect('select datasets', [1,2,3])
+st.write('test')
+
+with col1:
+    el1 = st.selectbox('x-axis', cat_names)
+    el2 = st.selectbox('y-axis', cat_names)
+    
+with col2:
+    fig = plt.figure()
+    plt.scatter(df[el1]/10000, df[el2]/10000)
+    st.pyplot(fig)
 
 st.dataframe(df)
 
