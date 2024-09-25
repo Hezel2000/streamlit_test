3c89dcd9e10f9dbbb5929af4365f7c970a3301ab
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2024-09-25 09:00:55 +0000

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,15 @@ st.title('NFDI4Earth Streamlit Test Page')
 
 df = pd.read_csv('data/Bastar Craton.csv')
 
+elements = df.columns.tolist()[27:]
+
+x_axis_el = st.selectbox('x-axis', elements)
+y_axis_el = st.selectbox('y-axis', elements)
+
 fig = plt.figure()
-plt.scatter(df['Mg'], df['Si'])
+plt.scatter(df[x_axis_el]/10000, df[y_axis_el]/10000)
+plt.xlabel(f'{x_axis_el} (wt%)')
+plt.ylabel(f'{y_axis_el} (wt%)')
 st.pyplot(fig)
 
 st.dataframe(df)
