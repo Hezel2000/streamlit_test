f80d54058863c766c3ad05aedbae980223402d1d
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2024-09-25 09:11:36 +0000

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,25 +4,31 @@ import matplotlib.pyplot as plt
 
 st.title('NFDI4Earth Streamlit Test Page')
 
-df = pd.read_csv('data/Bastar Craton.csv')
+passwd = st.text_input('input password')
 
-elements = df.columns.tolist()[27:]
+if passwd == '123password':
+    df = pd.read_csv('data/Bastar Craton.csv')
 
-tab1, tab2 = st.tabs(['Plot', 'Data'])
+    elements = df.columns.tolist()[27:]
 
-with tab1:
-    col1, col2 = st.columns([20, 80])
+    tab1, tab2 = st.tabs(['Plot', 'Data'])
 
-    with col1:
-        x_axis_el = st.selectbox('x-axis', elements)
-        y_axis_el = st.selectbox('y-axis', elements)
+    with tab1:
+        col1, col2 = st.columns([20, 80])
 
-    with col2:
-        fig = plt.figure()
-        plt.scatter(df[x_axis_el]/10000, df[y_axis_el]/10000)
-        plt.xlabel(f'{x_axis_el} (wt%)')
-        plt.ylabel(f'{y_axis_el} (wt%)')
-        st.pyplot(fig)
+        with col1:
+            x_axis_el = st.selectbox('x-axis', elements)
+            y_axis_el = st.selectbox('y-axis', elements)
 
-with tab2:
-    st.dataframe(df)
+        with col2:
+            fig = plt.figure()
+            plt.scatter(df[x_axis_el]/10000, df[y_axis_el]/10000)
+            plt.xlabel(f'{x_axis_el} (wt%)')
+            plt.ylabel(f'{y_axis_el} (wt%)')
+            st.pyplot(fig)
+
+    with tab2:
+        st.dataframe(df)
+
+else:
+    st.write('password is wrong')
