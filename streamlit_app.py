051bdf45d173c229604faac4aa89f55b6ccb8a9b
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2024-09-25 08:50:49 +0000

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title('Plot Georoc Datasets')
-st.write('*NFDI4Earth Academy Example*')
+st.title('NFDI4Earth Streamlit Test Page')
 
 df = pd.read_csv('data/Bastar Craton.csv')
-cat_names = df.columns.tolist()[27:]
 
-st.multiselect('select datasets', [1,2,3])
-
-col1, col2 = st.columns(2)
-
-with col1:
-    el1 = st.selectbox('x-axis', cat_names)
-    el2 = st.selectbox('y-axis', cat_names)
-    
-with col2:
-    fig = plt.figure()
-    plt.scatter(df[el1]/10000, df[el2]/10000)
-    st.pyplot(fig)
+st.dataframe(df)
