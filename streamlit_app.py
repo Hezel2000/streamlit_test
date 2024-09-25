7c07c896d1337d29247b52db5767aee4513224d1
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2024-09-25 09:05:08 +0000

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,32 @@ df = pd.read_csv('data/Bastar Craton.csv')
 
 elements = df.columns.tolist()[27:]
 
-x_axis_el = st.selectbox('x-axis', elements)
-y_axis_el = st.selectbox('y-axis', elements)
 
-fig = plt.figure()
-plt.scatter(df[x_axis_el]/10000, df[y_axis_el]/10000)
-plt.xlabel(f'{x_axis_el} (wt%)')
-plt.ylabel(f'{y_axis_el} (wt%)')
-st.pyplot(fig)
+col1, col2 = st.columns([20, 80])
+
+with col1:
+    x_axis_el = st.selectbox('x-axis', elements)
+    y_axis_el = st.selectbox('y-axis', elements)
+
+with col2:
+    fig = plt.figure()
+    plt.scatter(df[x_axis_el]/10000, df[y_axis_el]/10000)
+    plt.xlabel(f'{x_axis_el} (wt%)')
+    plt.ylabel(f'{y_axis_el} (wt%)')
+    st.pyplot(fig)
+
 
 st.dataframe(df)
 
+tab1, tab2 = st.tabs(['Plot', 'Data'])
+
+with tab1:
+    x_axis_el = st.selectbox('x-axis', elements)
+    y_axis_el = st.selectbox('y-axis', elements)
+
+with tab2:
+    fig = plt.figure()
+    plt.scatter(df[x_axis_el]/10000, df[y_axis_el]/10000)
+    plt.xlabel(f'{x_axis_el} (wt%)')
+    plt.ylabel(f'{y_axis_el} (wt%)')
+    st.pyplot(fig)
